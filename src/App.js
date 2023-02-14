cb13d03030c1a6edd39ff37c0ad06a4b22d6596a
Grace Lee <gracelee9901@gmail.com>
2023-02-14 15:37:42 -0800

    React router setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
+import { Routes, Route } from "react-router-dom";
 
 function App() {    
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Routes>
+                <Route exact path="/" element={<Photos />}>
+                </Route>
+                <Route path="/cart" element={<Cart />}>
+                </Route>
+            </Routes>
         </div>
     )
 };
