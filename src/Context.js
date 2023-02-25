96430646e1fd421ce7d53306651ab83964254c0c
Grace Lee <gracelee9901@gmail.com>
2023-02-24 23:13:40 -0800

    Fetch and display images

--- src/Context.js
+++ src/Context.js
@@ -4,6 +4,14 @@ const Context = React.createContext()
 
 function ContextProvider({children}) {
     const [photos, setPhotos] = useState([]);
+
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setPhotos(data));
+    }, []);
+
     return (
         <Context.Provider value={{photos}}>
             {children}
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "../components/Image";
+import { Context } from "../Context";
 
 function Photos() {
+    const {photos} = useContext(Context);
+
+    const imageElements = photos.map((img, i) => (
+        <Image key={img.id} img={img} className={getClass(i)} />
+    ))
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {imageElements}
         </main>
     )
 };
