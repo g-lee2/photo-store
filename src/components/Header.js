80ff34107faa281092069da0c8a194c1e915710d
Grace Lee <gracelee9901@gmail.com>
2023-02-25 16:05:34 -0800

    Show filled cart icon in header if there are images in cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
+
 
 function Header() {
+    const {cartItems} = useContext(Context);
+
+    function cartIcon() {
+        if (cartItems.length > 0) {
+            return "ri-shopping-cart-fill ri-fw ri-2x";
+        } else {
+            return "ri-shopping-cart-line ri-fw ri-2x";
+        }
+    }
+
     return (
         <header>
             <h2><Link to="/">Pic Some</Link></h2>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart"><i className={cartIcon()}></i></Link>
         </header>
     )
 };
